b18d208ae06d8000490ff8ff08c94e7209ce8cc1
opiepj <opiepj@plu.edu>
2015-09-02 21:33:05 -0700

    Fix modal and navbar. Go TEAM

--- public/scripts.js
+++ public/scripts.js
@@ -10,19 +10,19 @@ module.controller = function () {
         return m('.#modal.modal-wrapper', [
 					m('.modal-container', [
 						m('form.modal-form', [
-							m('h2', New Module),
+							m('h2', 'New Module'),
 							m('p', JSON.stringify(shortcut,null, 4)),
 							m('.input-group', [
 								m('p', [
 									m('input[type=text]')
 								]),
 								m('p', [
-									m('input[type=submit]', Create)
+									m('input[type=submit]', 'Create')
 								])
 							])
-						])
-					]),
-					m('a.modal-close[href="#closed"]', X) 
+						]),
+						m('a.modal-close[href="#closed"]', 'X') 
+					])
 				]);
       } else {
         return '';
@@ -34,7 +34,7 @@ module.view = function (vm) {
   return [
 		m('.shortcut-container', [
 			shortcuts.map(function (shortcut) {
-        return m('a.modal-line[href="#modal]', {
+        return m('a.modal-link[href="#modal"]', {
           onclick: shortcut.modal.visible.bind(this, true)
         }, shortcut.name);
       })
