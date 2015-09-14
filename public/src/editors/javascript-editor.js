68c9ca43574505ac34248828770639359a38ee6b
opiepj <opiepj@plu.edu>
2015-09-14 08:21:29 -0700

    (#20) ace editor implementation with javascript editor, this can be dynamic.

--- public/src/editors/javascript-editor.js
+++ public/src/editors/javascript-editor.js
@@ -1,17 +1,3 @@
-var ace = require('brace');
-require('brace/mode/javascript');
-require('brace/theme/monokai');
-require('brace/keybinding/vim');
-
-var editor = ace.edit('javascript-editor');
-editor.getSession().setMode('ace/mode/javascript');
-editor.setTheme('ace/theme/monokai');
-editor.setKeyboardHandler('ace/keyboard/vim');
-editor.setValue([
-  '// JavaScript',
-  'var a = 3;',
-  '',
-  '// below line has an error which is annotated',
-  'var b ='
-].join('\n'));
-editor.clearSelection();
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/twilight");
+editor.getSession().setMode("ace/mode/javascript");
