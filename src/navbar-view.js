3f7acdec841129f5e01db48caac35c9317903474
Tom Manion <thomaswmanion@gmail.com>
2015-10-28 23:38:24 -0700

    More styling updates.

--- src/navbar-view.js
+++ src/navbar-view.js
@@ -1,14 +1,13 @@
 var Masonry = require('masonry-layout');
 var m = require('mithril');
 var inputs = require('./inputs');
-function config(el, isInit, ctx) {
-  var setup = function() {
-    var msnry = new Masonry(el, {
-      itemSelector: '.masonry-brick',
-      transitionDuration: '0.5s'
-    });
-  };
-  setTimeout(setup, 1000);
+
+function config(el) {
+  new Masonry(el, {
+    itemSelector: '.masonry-brick',
+    transitionDuration: '0.5s',
+    columnWidth: 250
+  });
 }
 
 function openShortcut(shortcut, that) {
@@ -29,25 +28,27 @@ module.exports = function(vm) {
   return [
         m('.shortcut-container', [
             m('.grid', {
-                config: config
-            }, vm.shortcuts().map(function(shortcut) {
-                return m('.col-xs-12.col-sm-6.col-md-4.col-lg-3.masonry-brick.card.card-block.masonry-brick', [
-                    m('.card-contents', [
-                      shortcut.form.map(function(field) {
-                            return m('.group', [
-                                m('label[for=' + field.id + ']', field.label),
-                                m('br'),
-                                inputs.createInput(field),
-                                m('hr')
-                            ]);
-                        }),
-                        m('input.btn.btn-md.full-width[type=submit]', {
-                            onclick: vm.submit(shortcut),
-                            value: shortcut.name
-                        })
-                    ])
-                ]);
-            }))
-        ])
-    ];
+        config: config
+      }, vm.shortcuts().map(function(shortcut) {
+        return m('.masonry-brick', [
+          m('.masonry-inner', [
+            m('h2', shortcut.name),
+            m('hr'),
+            shortcut.form.map(function(field) {
+              return m('.group', [
+                m('label[for=' + field.id + ']', field.label),
+                m('br'),
+                inputs.createInput(field),
+                m('hr')
+              ]);
+            }),
+            m('input.btn.btn-primary.btn-md.full-width[type=submit]', {
+              onclick: vm.submit(shortcut),
+              value: 'Run'
+            })
+          ])
+        ]);
+      }))
+    ])
+  ];
 };
